9e384bde89bc0b6b3b27559be2159732b7167eee
Ujjwal Solanki <75205571+ujjwalsolankii@users.noreply.github.com>
2023-07-10 00:02:32 +0530

    Time: 4 ms (62.75%), Space: 43.7 MB (95.77%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.java
+++ 0137-single-number-ii/0137-single-number-ii.java
@@ -1,15 +1,18 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        Map<Integer, Integer> map = new HashMap<>();
-        for (int i : nums) {
-            map.put(i, map.getOrDefault(i, 0) + 1);
+        int n = nums.length;
+        if(n<3){
+            return nums[0];
         }
-        for (int i : map.keySet()) {
-            if (map.get(i) == 1) {
-                return i;
+        Arrays.sort(nums);
+        if(nums[0] != nums[1]) return nums[0];
+        int i=1;
+        while(i<n){
+            if(nums[i]!=nums[i-1]){
+                return nums[i-1];
             }
+            i+=3;
         }
-        return 0;
-        
+        return nums[n-1];
     }
 }
