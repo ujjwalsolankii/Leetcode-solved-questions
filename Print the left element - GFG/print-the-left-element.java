8efdd0c2d9237f0dd96965b10bde29e10eb4c76a
Ujjwal Solanki <75205571+ujjwalsolankii@users.noreply.github.com>
2023-07-31 19:52:24 +0530

    Added solution - LeetHub

---
+++ Print the left element - GFG/print-the-left-element.java
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+	public static void main(String[] args) throws IOException
+	{
+	        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t =
+            Integer.parseInt(br.readLine().trim()); // Inputting the testcases
+        while(t-->0)
+        {
+            long n = Long.parseLong(br.readLine().trim());
+            long a[] = new long[(int)(n)];
+            // long getAnswer[] = new long[(int)(n)];
+            String inputLine[] = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                a[i] = Long.parseLong(inputLine[i]);
+            }
+            
+            Solution obj = new Solution();
+            System.out.println(obj.leftElement(a, n));
+            
+        }
+	}
+}
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+class Solution {
+    
+    public long leftElement(long arr[], long n)
+    {
+        // Your code goes here  
+        Arrays.sort(arr);
+        int mid = ((int)n-1)/2;
+        long a = arr[mid];
+        return a;
+    }
+}
